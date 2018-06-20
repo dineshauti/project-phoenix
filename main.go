e43fc57d428e4777f8d2804a064d98ba9c55f502
dineshauti <auti.dinesh3@gmail.com>
2018-06-19 20:35:27 -0400

    main.go changed

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "This is amazing! Served from inside a docker container :)")
+	fmt.Fprintln(w, "Phoenix pipeline has been deployed!")
 }
 
 func main() {
